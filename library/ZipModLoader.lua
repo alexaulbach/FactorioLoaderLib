119718641c31e845b2c9b8a7d7b5f78f282bada6
RustyBlade64 <43153542+RustyBlade64@users.noreply.github.com>
2019-04-21 19:07:53 +0200

    Support require() relative to requiring file
    Factorio allows requiring files relative to the file doing the require call.
    For example given the scenario
    
    modname_1.2.3/
    `-- prototypes
        |-- a.lua
        `-- b.lua
    
    both of the following require() calls are a valid way
    to require file b from file a:
    
    a) require("prototypes.b")
    b) require("b")
    
    This behaviour is different from default Lua, where require-calls must be
    absolute to the base folder and where only case a) is valid.
    
    This commit stores each file's directory in a stack as they are executed
    and then tries to require the file both relative to the top of the
    stacktrace and absolute to the root folder of the zip archive.

--- library/ZipModLoader.lua
+++ library/ZipModLoader.lua
@@ -1,6 +1,8 @@
 local ZipModLoader = {}
 ZipModLoader.__index = ZipModLoader
 
+local directory_stack = {}
+
 function ZipModLoader.new(dirname, mod_name, arc_subfolder)
     local filename = dirname .. mod_name .. ".zip"
     local arc = assert(zip.open(filename))
@@ -15,14 +17,31 @@ end
 
 function ZipModLoader:__call(name)
     name = string.gsub(name, "%.", "/")
-    local filename = self.arc_subfolder .. name .. ".lua"
-    local file = self.archive:open(filename)
+    local potential_files = { self.arc_subfolder .. name .. ".lua" }
+    if #directory_stack ~= 0 then
+        table.insert(potential_files, directory_stack[#directory_stack] .. name .. ".lua")
+    end
+    local file = nil
+    local filename = nil
+    for _, f in ipairs(potential_files) do
+        file = self.archive:open(f)
+        if file then
+            filename = f
+            break
+        end
+    end
     if not file then
-        return "Not found: " .. filename .. " in " .. self.archive_name
+        return "Not found: " .. name .. " in " .. self.archive_name
     end
     local content = file:read("*a")
     file:close()
-    return load(content, filename)
+    local loaded_chunk = load(content, filename)
+    return function()
+        table.insert(directory_stack, filename:sub(1, filename:find("/[^/]*$")))
+        local result = loaded_chunk()
+        table.remove(directory_stack)
+        return result
+    end
 end
 
 function ZipModLoader:close()
